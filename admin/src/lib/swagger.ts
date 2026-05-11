bcd65c3a8ac50809e5a19e0d2380b5bd5da7e409
manfuji <telebrytt@gmail.com>
2026-05-11 13:52:20 +0000

    updated the code

--- admin/src/lib/swagger.ts
+++ admin/src/lib/swagger.ts
@@ -1,25 +1,53 @@
-import { createSwaggerSpec } from 'next-swagger-doc';
+import { createSwaggerSpec } from "next-swagger-doc";
+
+const URL_PARSE_DEPRECATION_CODE = "DEP0169";
+
+function ignoreSwaggerUrlParseWarning<T>(callback: () => T): T {
+  const emitWarning = process.emitWarning;
+
+  process.emitWarning = ((warning, ...args) => {
+    const [typeOrOptions, code] = args;
+    const warningCode =
+      typeof typeOrOptions === "object" && typeOrOptions !== null
+        ? typeOrOptions.code
+        : code;
+
+    if (warningCode === URL_PARSE_DEPRECATION_CODE) {
+      return;
+    }
+
+    return Reflect.apply(emitWarning, process, [warning, ...args]);
+  }) as typeof process.emitWarning;
+
+  try {
+    return callback();
+  } finally {
+    process.emitWarning = emitWarning;
+  }
+}
 
 export const getApiDocs = async () => {
-  const spec = createSwaggerSpec({
-    apiFolder: 'src/app/api',
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'Athletic Agency API Documentation',
-        version: '1.0',
-      },
-      components: {
-        securitySchemes: {
-          BearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
+  const spec = ignoreSwaggerUrlParseWarning(() =>
+    createSwaggerSpec({
+      apiFolder: "src/app/api",
+      definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "Athletic Agency API Documentation",
+          version: "1.0",
+        },
+        components: {
+          securitySchemes: {
+            BearerAuth: {
+              type: "http",
+              scheme: "bearer",
+              bearerFormat: "JWT",
+            },
           },
         },
+        security: [],
       },
-      security: [],
-    },
-  });
+    }),
+  );
   return spec;
 };
